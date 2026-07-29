7dd6c6b70d4ffa0f4eaa9c07f9da9434d0f69e7f
Thien-Nguyen-1 <thienbnguyen.programming@gmail.com>
2026-07-29 16:24:04 +0100

    Implemented analyser for data loss

---
+++ DataLoss_Analyser.py
@@ -0,0 +1,72 @@
+#Import required modules for constructing and training the model.
+import torch
+from torch.utils.data import ConcatDataset
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import json
+
+
+import ast
+
+# Device and system imports 
+import sys
+import random
+import argparse
+import os
+from pathlib import Path
+import re
+import numpy as np
+
+#imports required for data logging
+import json
+import wandb
+from datetime import datetime 
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    #Required argument to load in JSON file of results
+    parser.add_argument('-d', 
+                        '--data-path', 
+                        dest='data_path', 
+                        required = True, 
+                        type = str, 
+                        nargs = "+",
+                        help='Input')
+    args = parser.parse_args()
+   
+    
+    
+
+
+
+def process_training_log(dataset_paths):
+
+    all_training_err = []
+
+
+    for path in dataset_paths:
+        
+
+        with open(path, "r") as file:
+            data = json.load(file)
+
+        all_training_err.append([epoch['train_err'] for epoch in data])
+            
+
+        
+    #set up line chart to display data
+    x_axis = np.arange(0, len(all_training_err[0]))
+        
+    plt.plot(x_axis, all_training_err[0], color="red")
+    plt.plot(x_axis, all_training_err[1], color = "green")
+    plt.ylabel("training error")
+    plt.xlabel("epoch")
+    plt.show()
+        
+
+
+if len(args.data_path) == 2:
+    process_training_log(args.data_path)
+
